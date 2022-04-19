02526ab428071c2231a8ff52219e89009fe77f05
Aneesh AV <aneesh@amcde.com>
2022-04-19 14:55:37 +0300

    [IMP] Code update

--- kfmi_access_user_wh/models/warehouse.py
+++ kfmi_access_user_wh/models/warehouse.py
@@ -4,15 +4,11 @@ from odoo import models, fields
 class WareHouseInherit(models.Model):
     _inherit = 'stock.warehouse'
 
-    user_ids = fields.Many2many(comodel_name='res.users', string='User')
+    user_ids = fields.Many2many('res.users', string='User')
 
 
 class StockPickingTypeInherit(models.Model):
     _inherit = 'stock.picking.type'
 
-    user_ids = fields.Many2many(comodel_name='res.users', string='User')
-
-
-class StockTypeInherit(models.Model):
-    _inherit = 'stock.picking'
+    user_ids = fields.Many2many('res.users', string='User')
 
