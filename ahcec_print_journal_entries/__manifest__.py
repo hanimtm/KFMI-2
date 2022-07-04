89918cb7891c8d1b2354f6cf7ef91c81a4a1b0c0
Aneesh AV <aneesh@amcde.com>
2022-07-04 11:49:57 +0300

    [ADD]

--- ahcec_print_journal_entries/__manifest__.py
+++ ahcec_print_journal_entries/__manifest__.py
@@ -2,9 +2,7 @@
 {
 	"name": "Print Journal Entries ",
 	"author": "Aneesh.AV",
-	"website": "https:www.softhealer.com",
-	"support": "support@softhealer.com",
-	"version" : "13.0.1",
+	"version" : "15.0.1",
 	"category": "Accounting",
 	"summary": """print journal report app, print multiple journal module, print journal entry odoo""",
 	"description": """This module useful to print journal entries.
@@ -17,4 +15,5 @@ print journal report app, print multiple journal module, print journal entry odo
 	"installable": True,
 	"application": True,
 	"auto_install": False,
+	"license": "LGPL-3",
 }
